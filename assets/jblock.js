2c4bf34e81a198c9b70efea5b29dac93b430bdd5
Joachim Doerr <mail@joachim-doerr.com>
2016-08-05 16:12:09 +0200

    finalize javascript for default functionality

--- assets/jblock.js
+++ assets/jblock.js
@@ -40,12 +40,68 @@ function sortit(element) {
 
 function reindexit(element) {
     element.find('> div').each(function(index) {
+
+        if (index == 0) {
+            $(this).find('.removeme').hide();
+        } else {
+            $(this).find('.removeme').show();
+        }
+
         // find input elements
-        $(this).find('label,input,textarea,select').each(function() {
-            var value = $(this).attr('name').replace($(this).attr('name').match(/\]\[\d+\]\[/g), '][' + index + '][');
-            $(this).attr('name', value);
-            $(this).val(value);
+        $(this).find('input,textarea,select').each(function() {
+            var attr = $(this).attr('name');
+            // For some browsers, `attr` is undefined; for others,
+            // `attr` is false. Check for both.
+            if (typeof attr !== typeof undefined && attr !== false) {
+                var value = $(this).attr('name').replace($(this).attr('name').match(/\]\[\d+\]\[/g), '][' + index + '][');
+                $(this).attr('name', value);
+            }
+
+            replacefor(element, $(this), index);
+
+            // select rex button
+            if ($(this).prop("nodeName") == 'SELECT' && (
+                    $(this).attr('id').indexOf("REX_MEDIALIST_SELECT_") >= 0 ||
+                    $(this).attr('id').indexOf("REX_LINKLIST_SELECT_") >= 0
+                )) {
+                $(this).attr('id', $(this).attr('id').replace(/\d+/, index));
+                if ($(this).attr('name').length) {
+                    $(this).attr('name', $(this).attr('name').replace(/\d+/, index));
+                }
+            }
+
+            // input rex button
+            if ($(this).prop("nodeName") == 'INPUT' && (
+                    $(this).attr('id').indexOf("REX_LINK_") >= 0 ||
+                    $(this).attr('id').indexOf("REX_LINKLIST_") >= 0 ||
+                    $(this).attr('id').indexOf("REX_MEDIA_") >= 0 ||
+                    $(this).attr('id').indexOf("REX_MEDIALIST_") >= 0
+                )) {
+                $(this).attr('id', $(this).attr('id').replace(/\d+/, index));
+                // button
+                $(this).parent().find('a.btn-popup').each(function(){
+                    $(this).attr('onclick', $(this).attr('onclick').replace(/\(\d+/, '(' + index));
+                    $(this).attr('onclick', $(this).attr('onclick').replace(/_\d+/, '_' + index));
+                });
+            }
         });
+    });
+}
+
+function replacefor(element, item, index) {
+    element.find('label').each(function() {
+        if ($(this).attr('for') == item.attr('id')) {
+            var id = $(this).attr('for') + '_' + index;
+            $(this).attr('for', id);
+            item.attr('id', id);
+        }
+    });
+    replacecheckboxfor(element);
+}
+
+function replacecheckboxfor(element) {
+    element.find('input:checkbox').each(function() {
+        $(this).parent().find('label').attr('for', $(this).attr('id'));
     });
 }
 
@@ -74,8 +130,10 @@ function removeitem(element, item) {
 function addlinking(element) {
     element.find('> div .addme').unbind().bind('click', function() {
         additem(element, $(this).closest('div[class^="sortitem"]'));
+        return false;
     });
     element.find('> div .removeme').unbind().bind('click', function() {
         removeitem(element, $(this).closest('div[class^="sortitem"]'));
+        return false;
     });
 }
