3d05a56d37a831a9d19006968141aefb157b1727
Joachim Doerr <mail@joachim-doerr.com>
2016-08-13 12:40:35 +0200

    add theme css

--- boot.php
+++ boot.php
@@ -1,25 +1,24 @@
 <?php
 
-if (rex_addon::get('assets')->isAvailable()) {
-    rex_extension::register('BE_ASSETS', function ($ep) {
-        $Subject = $ep->getSubject() ? $ep->getSubject() : [];
-        $Subject[$this->getPackageId()] = [
-            'files' => [
-                $this->getPath('assets/styles.less'),
-                $this->getPath('assets/slice_skin.js'),
-            ],
-            'addon' => $this->getPackageId(),
-        ];
-        return $Subject;
-    }, rex_extension::EARLY);
-} elseif (rex::isBackend()) {
-    rex_view::addCssFile($this->getAssetsUrl('styles.less.min.css'));
-    rex_view::addJsFile($this->getAssetsUrl('slice_skin.jsmin.min.js'));
+if (rex::isBackend() && is_object(rex::getUser())) {
+//    echo '<pre>';
+//    print_r(rex_addon::get('jblock')->getConfig('jblock_theme'));
+//    echo '</pre>';
+    // check theme css is exists
+    JBlockThemeHelper::themeBootCheck(rex_addon::get('jblock')->getConfig('jblock_theme'));
+
+    // use theme helper class
+    if (JBlockThemeHelper::getCssAssets(rex_addon::get('jblock')->getConfig('jblock_theme'))) {
+        // foreach all css files
+        foreach (JBlockThemeHelper::getCssAssets(rex_addon::get('jblock')->getConfig('jblock_theme')) as $css) {
+            // add assets css file
+            rex_view::addCssFile($this->getAssetsUrl($css));
+        }
+    }
     rex_view::addJsFile($this->getAssetsUrl('jquery.fn.sortable.min.js'));
     rex_view::addJsFile($this->getAssetsUrl('jblock.js'));
     rex_view::addCssFile($this->getAssetsUrl('jblock.css'));
+
+//    rex_perm::register('slice_ui[json]', null, rex_perm::OPTIONS);
+
 }
-
-if (rex::isBackend() && is_object(rex::getUser()))
-    rex_perm::register('slice_ui[json]', null, rex_perm::OPTIONS);
-
