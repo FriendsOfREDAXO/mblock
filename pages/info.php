53e5f27f426d2d3d357b2015b235b6fa2d99f071
Joachim Doerr <mail@joachim-doerr.com>
2016-08-18 13:39:01 +0200

    update lang and info

--- pages/info.php
+++ pages/info.php
@@ -10,16 +10,14 @@
 
 if (rex_addon::isInstalled('mform') !== true) {
     $content = '
-        <h3>MForm</h3>
-        <p>Dieses Addon erleichtert das Erstellen von Modul Input Formularen.</p>
+        <h3>JBlock</h3>
+        <p>Dieses Addon ermöglicht es Nested-Slices zu erstellen.</p>
     ';
 } else {
-    $headline = '<h3>'. $this->i18n('help_subheadline_1') .'</h3>';
+    $headline = '<h3>'. rex_i18n::msg('jblock_help_subheadline_1') .'</h3>';
     $content = '
-        <p>'.  $this->i18n('help_infotext_1') .'</p>
-        <p>'.  $this->i18n('help_infotext_2') .'</p>
-        <p>'.  $this->i18n('help_infotext_3') .'</p>
-        <a href="https://github.com/FriendsOfREDAXO/mform/wiki" target="_blank">'. $this->i18n('mform_github') .'</a>
+        <p>'.  rex_i18n::msg('jblock_help_infotext_1') .'</p>
+        <p>'.  rex_i18n::msg('jblock_help_infotext_2') .'</p>
+        <a href="https://github.com/FriendsOfREDAXO/jblock/" target="_blank">'. rex_i18n::msg('jblock_github') .'</a>
     ';
-    $content .= '<br><a href="https://github.com/FriendsOfREDAXO/mform/wiki" target="_blank">'. $this->i18n('wiki') .'</a>';
 }
