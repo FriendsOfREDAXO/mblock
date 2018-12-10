d6d1fe8416abe2383f1b0cc33b4b297cffcfef38
Thomas Skerbis <thomas.skerbis@mac.com>
2018-12-10 21:22:45 +0100

    rex_form demo install by rex_sql_table

--- install.php
+++ install.php
@@ -20,4 +20,14 @@ if (!$this->hasConfig('mblock_scroll')) {
 rex_dir::copy($this->getPath('data'), $this->getDataPath());
 
 // add demo table
-rex_sql_util::importDump($this->getPath('rexform_demo.sql'));
+rex_sql_table::get(rex::getTable('mblock_rexform_demo'))
+    ->ensureColumn(new rex_sql_column('id', 'int(11)', false, null, 'auto_increment'))
+    ->ensureColumn(new rex_sql_column('status', 'int(1)', true, '1'))
+    ->ensureColumn(new rex_sql_column('name', 'text'))
+    ->ensureColumn(new rex_sql_column('mblock_field', 'text'))
+    ->ensureColumn(new rex_sql_column('createdate', 'datetime', true))
+    ->ensureColumn(new rex_sql_column('updatedate', 'datetime', true))
+    ->ensureColumn(new rex_sql_column('createuser', 'varchar(255)'))
+    ->ensureColumn(new rex_sql_column('updateuser', 'varchar(255)'))
+    ->setPrimaryKey('id')
+    ->ensure();
