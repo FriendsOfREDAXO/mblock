47cc2605cb7b3d3cccb445c56e542337e4b2ad94
Joachim Dörr <mail@joachim-doerr.com>
2019-04-19 11:24:36 +0200

    use span, add visibility button

--- fragments/default_theme/element.php
+++ fragments/default_theme/element.php
@@ -1,14 +1,11 @@
 <div class="sortitem" data-mblock-iterate-index="<?= $this->iterate_index; ?>" data-default-count="<?= $this->iterate_index-1; ?>">
     <span class="sorthandle"></span>
     <span class="removeadded">
-      <div class="btn-group btn-group-xs">
-         <button type="button" class="btn btn-default addme" title="duplicate"><i class="rex-icon rex-icon-add-module"></i></button>
-         <button type="button" class="btn btn-delete removeme" title="delete"><i class="rex-icon rex-icon-delete"></i></button>
-      </div>
-      <div class="btn-group btn-group-xs">
-         <button type="button" class="btn btn-move moveup" title="move up"><i class="rex-icon rex-icon-up"></i></button>
-         <button type="button" class="btn btn-move movedown" title="move down"><i class="rex-icon rex-icon-down"></i></button>
-      </div>
+        <span class="btn-default addme" title="duplicate"><i class="rex-icon rex-icon-add-module"></i></span>
+        <span class="btn-delete removeme" title="delete"><i class="rex-icon rex-icon-delete"></i></span>
+        <span class="btn-visibility visibility" title="visibility"><i class="rex-icon rex-icon-visible"></i></span>
+        <span class="btn-move moveup" title="move up"><i class="rex-icon rex-icon-up"></i></span>
+        <span class="btn-move movedown" title="move down"><i class="rex-icon rex-icon-down"></i></span>
     </span>
     <div class="mblock-sortitem-form"><?= $this->form; ?></div>
 </div>
