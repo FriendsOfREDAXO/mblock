724966054b6197da3e7e6d957e7393a9d3ff10c7
Norbert Micheel <nm@lautundschoen.de>
2023-02-01 12:31:54 +0100

    fix Fix: "iconv illegal characters found..."

--- lib/MBlock/Decorator/MBlockReplacerTrait.php
+++ lib/MBlock/Decorator/MBlockReplacerTrait.php
@@ -21,7 +21,7 @@ trait MBlockDOMTrait
     private static function createDom($html)
     {
         $dom = new DOMDocument();
-        $html = htmlspecialchars_decode(iconv('UTF-8', 'ISO-8859-1', htmlentities($html, ENT_COMPAT, 'UTF-8')), ENT_QUOTES);
+        $html = htmlspecialchars_decode(iconv('UTF-8', 'ISO-8859-1//IGNORE', htmlentities($html, ENT_COMPAT, 'UTF-8')), ENT_QUOTES);
         @$dom->loadHTML("<html xmlns=\"http://www.w3.org/1999/xhtml\"><body>$html</body></html>");
         $dom->preserveWhiteSpace = false;
         return $dom;
